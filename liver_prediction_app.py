4e8320c482d53de5165aff9d771eaee579398d99
Shubhamsharma0208 <136960076+Shubhamsharma0208@users.noreply.github.com>
2023-06-18 22:06:06 +0530

    Update liver_prediction_app.py

--- liver_prediction_app.py
+++ liver_prediction_app.py
@@ -10,14 +10,13 @@ from PIL import Image
 
 with st.sidebar:
         selected = option_menu(
-            menu_title = "Main Menu",options = ["Home","About","Contact"],
-        )
+            menu_title = "Main Menu",options = ["Home","About","Contact"],)
    
-    if selected == "Home":
-       st.title=(f"you have selected {selected}")
-    if selected == "About":
-       st.title=(f"you have selected {selected}")
-    if selected == "Contact":
+if selected == "Home":
+        st.title=(f"you have selected {selected}")
+if selected == "About":
+        st.title=(f"you have selected {selected}")
+if selected == "Contact":
         st.title=(f"you have selected {selected}")
  
 #load saved model
