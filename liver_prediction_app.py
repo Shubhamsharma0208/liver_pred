52021023a3e32b6d705d4de6dce34e08131ea468
Shubhamsharma0208 <136960076+Shubhamsharma0208@users.noreply.github.com>
2023-06-18 21:30:35 +0530

    Update liver_prediction_app.py

--- liver_prediction_app.py
+++ liver_prediction_app.py
@@ -79,7 +79,7 @@ def main():
 
     st.sidebar.header('Home')
 
-    vid=open("Healthy_liver_food.mov", 'rb')
+    vid=open("Healthy _liver_food.mov", 'rb')
     video = vid.read()
     st.video(video)
 
