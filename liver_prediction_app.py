f3d78c8906bb3aa738546e3b3b9308c192cc9ed5
Shubhamsharma0208 <136960076+Shubhamsharma0208@users.noreply.github.com>
2023-06-18 22:07:57 +0530

    Update liver_prediction_app.py

--- liver_prediction_app.py
+++ liver_prediction_app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import streamlit as st
+from streamlit import session_state as _state
 from sklearn.preprocessing import MinMaxScaler
 from streamlit_option_menu import option_menu
 import time
