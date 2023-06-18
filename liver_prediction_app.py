bffca3eecdd5c86e6237971814175508c8dfde5b
Shubhamsharma0208 <136960076+Shubhamsharma0208@users.noreply.github.com>
2023-06-18 22:22:35 +0530

    Update liver_prediction_app.py

--- liver_prediction_app.py
+++ liver_prediction_app.py
@@ -90,8 +90,9 @@ def main():
    
     with st.sidebar:
         selected = option_menu(
-            menu_title =None,options = ["Home","About","Contact"]
-   
+            menu_title =None,
+            options = ["Home","About","Contact"],
+        )
     if selected == "Home":
             st.title=(f"you have selected {selected}")
     if selected == "About":
