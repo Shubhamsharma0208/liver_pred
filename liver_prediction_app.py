a55fed6a64920f99a1f260145d528d502bff6dd8
Shubhamsharma0208 <136960076+Shubhamsharma0208@users.noreply.github.com>
2023-06-20 23:08:12 +0530

    Update liver_prediction_app.py

--- liver_prediction_app.py
+++ liver_prediction_app.py
@@ -46,7 +46,7 @@ def main():
     #getting input from user
     Age= st.text_input('Enter your Age')
 
-    st.write(___)
+    st.write("___")
     
     Gender = st.radio('Gender',('Male','Female'))
     if Gender=="Male":
